ff065566ccff4f4110d54440b27f1247a32609ce
Richard Cuario <richardcuario636@gmail.com>
2025-09-17 09:15:00 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+// src/App.js
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./auth/login";     // must match Login.js
+import Dashboard from "./page/dashboard";
+
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
